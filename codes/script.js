9d452329758dc462991717f78d58ae89efa32be3
Clarisse Campos <tetearruda17@gmail.com>
2021-01-11 18:13:23 -0300

    Primeiro commit

---
+++ codes/script.js
@@ -0,0 +1,33 @@
+let bearAud = document.getElementById("bearSound")
+let deerAud = document.getElementById("deerSound")
+let foxAud = document.getElementById("foxSound")
+let lionAud = document.getElementById("lionSound")
+let monkeyAud = document.getElementById("monkeySound")
+let owlAud = document.getElementById("owlSound")
+let parrotAud = document.getElementById("parrotSound")
+let sealAud = document.getElementById("sealSound")
+
+function bear(){
+    bearAud.play()
+}
+function deer(){
+    deerAud.play()
+}
+function fox(){
+    foxAud.play()
+}
+function lion(){
+    lionAud.play()
+}
+function monkey(){
+    monkeyAud.play()
+}
+function owl(){
+    owlAud.play()
+}
+function parrot(){
+    parrotAud.play()
+}
+function seal(){
+    sealAud.play()
+}
